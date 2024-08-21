9e10bf19251102017ace24c7310b795cddee021c
KMohZaid <68484509+KMohZaid@users.noreply.github.com>
2024-08-21 22:06:25 +0530

    ignore 'node_modules/' folder in telescope file browser

--- lua/custom/plugins/telescope-file-browser.lua
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -2,6 +2,13 @@ return {
   'nvim-telescope/telescope-file-browser.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
   config = function()
+    require('telescope').setup {
+      defaults = {
+        file_ignore_patterns = {
+          'node_modules',
+        },
+      },
+    }
     vim.keymap.set('n', '<space>e', function()
       require('telescope').extensions.file_browser.file_browser { cwd = vim.fn.expand '%:p:h' }
     end, { desc = 'File [E]xplorer' })

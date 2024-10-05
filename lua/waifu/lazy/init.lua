7b2a82810a57a7bfbc3a428c5b01d3900c5e2475
KMohZaid <68484509+KMohZaid@users.noreply.github.com>
2024-10-05 05:23:45 +0000

    move luals and luvit lsp config to lspconfig.lua

--- lua/waifu/lazy/init.lua
+++ lua/waifu/lazy/init.lua
@@ -1,21 +1,6 @@
 return {
 
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  -- LSP Plugins
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
-  { 'Bilal2453/luvit-meta', lazy = true },
-
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.

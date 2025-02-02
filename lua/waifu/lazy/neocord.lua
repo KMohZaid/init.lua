e300b3a6238d8e02721571b5b31da8b0825feec6
KMohZaid <68484509+KMohZaid@users.noreply.github.com>
2025-02-02 14:59:15 -0800

    add(neocord): discord rpc for neovim

---
+++ lua/waifu/lazy/neocord.lua
@@ -0,0 +1,8 @@
+return {
+  'IogaMaster/neocord',
+  event = 'VeryLazy',
+  config = function()
+    -- The setup config table shows all available config options with their default values:
+    require('neocord').setup {}
+  end,
+}

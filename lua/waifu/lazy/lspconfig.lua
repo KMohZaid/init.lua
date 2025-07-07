cd4b8e35525fe9e1a0c969b25d9fa44e258ca5e4
KMohZaid <68484509+KMohZaid@users.noreply.github.com>
2025-07-07 11:30:11 -0700

    lspconfig for vtsls

--- lua/waifu/lazy/lspconfig.lua
+++ lua/waifu/lazy/lspconfig.lua
@@ -169,7 +169,51 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+
+        -- INFO: `vtsls` config from https://www.lazyvim.org/extras/lang/typescript
+        vtsls = {
+          -- explicitly add default filetypes, so that we can extend
+          -- them in related extras
+          filetypes = {
+            'javascript',
+            'javascriptreact',
+            'javascript.jsx',
+            'typescript',
+            'typescriptreact',
+            'typescript.tsx',
+          },
+          settings = {
+            complete_function_calls = true,
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                maxInlayHintLength = 30,
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = 'always' },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = 'literals' },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+          keys = {
+            -- TODO: add keys, lazyvim had good one but for now they are conflicting with global keymap for goto definitiona and stuff. also LazyVim variable doesn't exist for me
+          },
+        },
+        eslint = {},
         --
 
         rust_analyzer = {
